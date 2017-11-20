05134cd702791cf3cde9d38b25a88ed7b7033669
goidelify <32669562+goidelify@users.noreply.github.com>
2017-11-20 18:01:42 +0000

    Added a lot of shit

--- gogn/script.js
+++ gogn/script.js
@@ -1,31 +1,37 @@
 init();
+var videos;
+
 function init(){
   getVideos(function(response) {
     // Parse JSON string into object
       var vids = JSON.parse(response);
+      videos = vids.videos;
       console.log(vids.categories, vids.videos);
-      setVideos(vids.categories, vids.videos);
+      setVideos(vids.categories);
    });
  }
 
 function getAge(age){
-  var ageInHours = age / 1000 / 3600;
+  var d = new Date();
+  var curTime = d.getTime();
+
+  var ageInHours = (curTime - age) / 1000 / 3600;
 
   if(ageInHours > 365*24)
-    return "Fyrir " + age / (365*24) + " ári/árum síðan";
+    return "Fyrir " + Math.round(ageInHours / (365*24)) + " ári/árum síðan";
   if(ageInHours > 30*24)
-    return "Fyrir " + age / (30*24)+ " mánuði/mánuðum síðan";
+    return "Fyrir " + Math.round(ageInHours / (30*24))+ " mánuði/mánuðum síðan";
   if(ageInHours > 7*24)
-    return "Fyrir " + age / (7*24) + " víku/víkum síðan";
+    return "Fyrir " + Math.round(ageInHours / (7*24)) + " víku/víkum síðan";
   if(ageInHours > 24)
-    return "Fyrir " + age / 24 + " degi/dögum síðan";
+    return "Fyrir " + Math.round(ageInHours / 24) + " degi/dögum síðan";
 
-  return "Fyrir " + age + " klukkustund/klukkustundum síðan"
+  return "Fyrir " + ageInHours + " klukkustund/klukkustundum síðan"
 }
 
 function getVideos(callback){
     var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
+    xobj.overrideMimeType("application/json");
     xobj.open('GET', 'videos.json', true);
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
@@ -36,6 +42,54 @@ function getVideos(callback){
 }
 
 
-function setVideos(categories, videos){
-  console.log(videos[0]);
+function setVideos(categories){
+  var col = document.createElement('div');
+  col.className = 'col';
+  for(var k = 0; k < categories.length; k++){
+    col.appendChild(addCategory(categories[k]));
+  }
+
+  document.body.appendChild(col);
+}
+
+function addCategory(category){
+  var row = document.createElement('div');
+  row.className = 'category row';
+
+  for(var i = 0; i < category.videos.length; i++){
+    var empty = document.createElement('div');
+    empty.className = 'col-2';
+
+    var newVid = addVideo(category.videos[i]-1);
+    row.appendChild(newVid);
+    if(i%2 == 0){
+      row.appendChild(empty);
+    }
+  }
+  return row;
+
+}
+
+function addVideo(id){
+  var vidDiv = document.createElement('div');
+  vidDiv.className = "col-5";
+
+  var vid = document.createElement('video');
+  vid.src = videos[id].video;
+  vid.poster = videos[id].poster;
+  vid.className = "video";
+
+  var vidName = document.createElement('p');
+  vidName.innerHTML = videos[id].title;
+  vidName.className = "caption";
+
+  var vidAge = document.createElement('p');
+  vidAge.innerHTML = getAge(videos[id].created);
+  vidAge.className = "age";
+
+  vidDiv.appendChild(vid);
+  vidDiv.appendChild(vidName);
+  vidDiv.appendChild(vidAge);
+
+  return vidDiv;
 }
